8154a6ad1fcaebe86a598ef40319b33f29de0062
BlakkTyger <himans23@iitk.ac.in>
2024-06-05 22:09:35 +0530

    minor bug fixes

--- web_server/target/debug/deps/web_server.d
+++ web_server/target/debug/deps/web_server.d
@@ -1,7 +1,5 @@
-C:\Users\Himan\Documents\Himanshu_Local\Summer'24\NFS\ass_repo\NFS_ass_Summer-24\web_server\target\debug\deps\web_server.exe: src\main.rs src\404.html src\500.html
+C:\Users\Himan\Documents\Himanshu_Local\Summer'24\NFS\ass_repo\NFS_ass_Summer-24\web_server\target\debug\deps\web_server.exe: src\main.rs
 
-C:\Users\Himan\Documents\Himanshu_Local\Summer'24\NFS\ass_repo\NFS_ass_Summer-24\web_server\target\debug\deps\web_server.d: src\main.rs src\404.html src\500.html
+C:\Users\Himan\Documents\Himanshu_Local\Summer'24\NFS\ass_repo\NFS_ass_Summer-24\web_server\target\debug\deps\web_server.d: src\main.rs
 
 src\main.rs:
-src\404.html:
-src\500.html:
